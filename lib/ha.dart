f938d0c7cb33b799b93e13eea514b15afa75fe4d
Mohan Gupta <mohan.kumar.gupta@gmail.com>
2023-06-27 09:24:21 +1000

    added tabs to findinf HA screen

--- lib/ha.dart
+++ lib/ha.dart
@@ -10,35 +10,82 @@ class HAScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
+    return const Material(
         child: Scaffold(
-      appBar: AppBar(
-        title: const Center(child: Text("Finding Home Assistant")),
-        backgroundColor: const Color(0xff764abc),
-      ),
-      body: const SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: EdgeInsets.only(
-                top: 30.0,
-                bottom: 10.0,
-                left: 20.0,
-              ),
-              child: Text(
-                "NEW",
-                style: TextStyle(fontSize: 16.0),
-              ),
-            ),
-            Flexible(
-              child: NewHAWidget(),
-            ),
-          ],
-        ),
+      //   appBar: AppBar(
+      //     title: const Center(child: Text("Finding Home Assistant")),
+      //     backgroundColor: const Color(0xff764abc),
+      //   ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Flexible(
+            child: HAInstancesWidget(),
+          ),
+        ],
       ),
     ));
+  }
+}
+
+class HAInstancesWidget extends StatefulWidget {
+  const HAInstancesWidget({super.key});
+
+  @override
+  State<HAInstancesWidget> createState() => _HAInstancesWidgetState();
+}
+
+class _HAInstancesWidgetState extends State<HAInstancesWidget>
+    with TickerProviderStateMixin {
+  late final TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Finding Home Assistant"),
+        backgroundColor: const Color(0xff764abc),
+        bottom: TabBar(
+            controller: _tabController,
+            indicatorColor: Colors.amberAccent,
+            indicatorWeight: 8,
+            tabs: const [
+              Tab(
+                text: "NEW",
+              ),
+              Tab(
+                text: "PREVIOUS",
+              ),
+              Tab(
+                text: "CONNECTED",
+              ),
+            ]),
+      ),
+      body: TabBarView(controller: _tabController, children: const [
+        Center(
+          child: NewHAWidget(),
+        ),
+        Center(
+          child: PreviousHAWidget(),
+        ),
+        Center(
+          child: ConnectedHAWidget(),
+        ),
+      ]),
+    );
   }
 }
 
@@ -56,6 +103,7 @@ class _NewHAWidgetState extends State<NewHAWidget> {
   void initState() {
     super.initState();
     //BlocProvider.of<HADiscoveredBloc>(context).add(FindHAInstancesEvent());
+    //TODO: BlocProvider.of<HADiscoveredBloc>(context).add(RestoreNewlyDiscoveredEvent());
   }
 
   @override
@@ -88,5 +136,33 @@ class _NewHAWidgetState extends State<NewHAWidget> {
                 );
               }),
     );
+  }
+}
+
+class PreviousHAWidget extends StatefulWidget {
+  const PreviousHAWidget({super.key});
+
+  @override
+  State<PreviousHAWidget> createState() => _PreviousHAWidgetState();
+}
+
+class _PreviousHAWidgetState extends State<PreviousHAWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class ConnectedHAWidget extends StatefulWidget {
+  const ConnectedHAWidget({super.key});
+
+  @override
+  State<ConnectedHAWidget> createState() => _ConnectedHAWidgetState();
+}
+
+class _ConnectedHAWidgetState extends State<ConnectedHAWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
