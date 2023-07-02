a679b214c0a94e4502a3f7b47f45762631de835b
Mohan Gupta <mohan.kumar.gupta@gmail.com>
2023-07-02 17:17:14 +1000

    my first passing widget tests

--- test/main_test.dart
+++ test/main_test.dart
@@ -10,10 +10,7 @@ import 'package:ha_assist/repository.dart';
 class MockHAConnectionBloc extends MockBloc<ConnectionStatusEvent, HAState>
     implements HAConnectionBloc {
   final MockHADiscoveredRepository repository;
-  MockHAConnectionBloc(this.repository) : super() {
-    on<ConnectionsPageLoad>(
-        (ConnectionStatusEvent event, Emitter<HAConnectionState> emit) {});
-  }
+  MockHAConnectionBloc(this.repository);
 }
 
 class MockHADiscoveredRepository extends Fake
@@ -25,14 +22,23 @@ void main() {
 
     testWidgets('Opening screen: HA disconnected and mic is off',
         (tester) async {
+      final MockHAConnectionBloc bloc =
+          MockHAConnectionBloc(MockHADiscoveredRepository());
+
+      whenListen(bloc, Stream.fromIterable([HAState()]),
+          initialState: HAState());
+
       await tester.pumpWidget(BlocProvider<HAConnectionBloc>(
-        create: (context) => MockHAConnectionBloc(MockHADiscoveredRepository()),
+        create: (context) => bloc,
         child: const MaterialApp(
           home: MainScreen(),
         ),
       ));
       await tester.pumpAndSettle();
-      expect(find.text("Disconnected"), findsOneWidget);
+      expect(find.text("Press the mic button and start speaking..."),
+          findsOneWidget);
+      expect(find.text("HA Connected"), findsNothing);
+      expect(find.text("HA Disconnected"), findsOneWidget);
     });
   });
 }
